306c4e505451ff60cab32dfcca49f9c4fc910195
mitchelle lindsey <mitchellelindsey@gmail.com>
2023-10-16 10:44:48 -0700

    conversion specifiers

---
+++ _print%.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * printf_c - prints % sign
+ * Return: int
+ */
+
+int printf_c(void)
+{
+	_putchar(37);
+	return (1);
+}
--- main.h
+++ main.h
@@ -8,5 +8,12 @@
 int _putchar(char c);
 int _printf(const char *format, ...);
 int putss(char *c);
+int printf_char(va_list val);
+int printf_string(va_list val);
+int _strlen(char *str);
+int _strlenc(const char *str);
+int printf_c(void);
+int print_dec(va_list args);
+int print_int(va_list args);
 
 #endif
